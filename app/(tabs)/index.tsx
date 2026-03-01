2db47e54f12115853273fa3686abd32725cfbf74
ishafii00 <sheyoday@gmail.com>
2026-03-01 10:59:55 -0500

    added index.tsx in app to show auth screen with no nav ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -74,12 +74,6 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-      {/* Temporarily view auth page */}
-        <Link href="/auth">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Click to View Auth Page</ThemedText>
-          </Link.Trigger>
-        </Link>
     </ParallaxScrollView>
   );
 }
