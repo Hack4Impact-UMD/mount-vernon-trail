2a6ca7665e1cfe921edf528863af729f4c053d9e
Chloe Thompson <tchloev@gmail.com>
2026-03-07 18:48:28 -0500

    card creation function for trello board

---
+++ trello-api/index.ts
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+import { TrelloClient } from "./trello-funcs";
+
+dotenv.config();
+
+const API_KEY = process.env.TRELLO_API_KEY;
+const API_TOKEN = process.env.TRELLO_API_TOKEN;
+
+if (!API_KEY || !API_TOKEN) {
+    console.error("Issue with API Key or Token");
+    process.exit(1);
+}
+
+const trello = new TrelloClient(API_KEY, API_TOKEN);
+
+async function Card(
+    boardName: string,
+    listName: string,
+    cardName: string,
+    cardDescription: string,
+) {
+    const boards = await trello.getBoards();
+    const targetBoard = boards.find((board) => board.name === boardName);
+
+    if (!targetBoard) {
+        console.error("board name not found");
+        return;
+    }
+
+    console.log(`\nfound board: ${targetBoard.name}`);
+
+    const lists = await trello.getLists(targetBoard.id);
+
+    if (lists.length > 0) {
+        const targetList = lists.find((list) => list.name === listName);
+
+        if (!targetList) {
+            console.error("list name not found");
+            return;
+        }
+
+        console.log(`found list: ${targetList.name}`);
+
+        await trello.createCard(targetList.id, cardName, cardDescription);
+        console.log(`success!`);
+    } else {
+        console.log("No lists in this board");
+    }
+}
+
+Card("MVT Mock Board", "Today", "clean up trail 5", "pick up trash");
