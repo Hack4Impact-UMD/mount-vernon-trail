44517f3afa3352e41b4a6161d0836db7782717c8
Ryan Zhao <ryanzhao1105@gmail.com>
2026-03-29 00:10:14 -0400

    add card drop shadow for android

--- components/ui/trail-issues-card.tsx
+++ components/ui/trail-issues-card.tsx
@@ -139,6 +139,7 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.08,
         shadowRadius: 12,
+        elevation: 3,
     },
     cardPressed: {
         opacity: 0.7,
